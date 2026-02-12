207251807bfc2e0e130fa2b6ec228471e2ba774f
PaulScott97 <paul-42@live.com.au>
2026-02-12 21:00:24 +1100

    building eisenhower view

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
+import { useState } from "react";
+
 function App() {
   return (
-    <>
-      <div>Hello, World!</div>
-    </>
+    <div className="container">
+      <Navbar />
+      <TasksView />
+    </div>
   );
+}
+
+function Navbar() {
+  return (
+    <nav>
+      <div>
+        <h2>Work</h2>
+        <hr />
+        <h2>Eisenhower</h2>
+      </div>
+      <button>+</button>
+    </nav>
+  );
+}
+
+function TasksView() {
+  const [view, setView] = useState("eisenhower");
+
+  if (view === "eisenhower") return <Eisenhower />;
+  if (view === "people") return <People />;
+}
+
+function Eisenhower() {
+  return (
+    <main className="eisenhower-view">
+      <Quadrant>urgent-important</Quadrant>
+      <Quadrant>not-urgent-important</Quadrant>
+      <Quadrant>urgent-not-important</Quadrant>
+      <Quadrant>not-urgent-not-important</Quadrant>
+    </main>
+  );
+}
+
+function Quadrant({ children }) {
+  return <section className={`quadrant ${children}`}>{children}</section>;
+}
+
+function People() {
+  return <div>People</div>;
 }
 
 export default App;
